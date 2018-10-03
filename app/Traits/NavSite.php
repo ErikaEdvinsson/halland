c49dcd9aa2d3c50e5bc789f3e757a7e79baa2a02
David Öhlin <hello@davidohl.in>
2018-10-03 08:33:29 +0200

    wip: real nav

--- app/Traits/NavSite.php
+++ app/Traits/NavSite.php
@@ -15,7 +15,10 @@ trait NavSite
 		if (!is_a($post, 'WP_Post')) {
 			return;
 		}
-	
+		
+		// Get main menu items
+		$menuItems = wp_get_nav_menu_items('main-menu');
+
 		// Get the highest ancestor of the current page
 		if (is_page() && !is_front_page() && !$post->post_parent) {
 			$top_page = $post->ID;
@@ -25,34 +28,32 @@ trait NavSite
 			$ancestors = get_post_ancestors($post->ID);
 			$top_page = array_values(array_slice($ancestors, -1))[0];
 		}
-		
-		// Get main menu items and frontpage
-		$menuItems = wp_get_nav_menu_items('main-menu');
+
 		$frontpage = (int)get_option('page_on_front');
+
+		$pages = [];
 
 		foreach ($menuItems as $i => $item) {
 			// Get the actual page ID that the menu item points to.
 			// https://wordpress.stackexchange.com/q/94787
 			$pageId = get_post_meta( $item->ID, '_menu_item_object_id', true );
+			$page = get_post($pageId);
 
-			if ($pageId === $frontpage) {
-				unset($pages[$i]);
-				continue;
-			}
-
-			if (isset($top_page) && $top_page === $pageId) {
+			if (isset($top_page) && $top_page === $page->ID) {
 				$page->active = true;
 			}
 
-			$item->children = get_pages(array( 
-				'child_of' => $pageId, 
-				'parent' => $pageId,
+			$page->children = get_pages(array( 
+				'child_of' => $page->ID, 
+				'parent' => $page->ID,
 				'hierarchical' => 0,
 				'sort_column' => 'menu_order', 
 				'sort_order' => 'asc'
 			));
+
+			$pages[] = $page;
 		}
 
-		return $menuItems;
+		return $pages;
 	}
 }
